adf34faebd1d9f2347b4cb6c06733ee886f52681
Luiz Romano <160535995+luizromanodev@users.noreply.github.com>
2025-10-22 15:53:20 -0300

    feat: correct integrations section animation

--- src/sections/Integrations.tsx
+++ src/sections/Integrations.tsx
@@ -64,6 +64,7 @@ export default function Integrations() {
                             <IntegrationsColumn integrations={integrations} />
                             <IntegrationsColumn
                                 integrations={integrations.slice().reverse()}
+                                reversed
                                 className="hidden md:flex"
                             />
                         </div>
